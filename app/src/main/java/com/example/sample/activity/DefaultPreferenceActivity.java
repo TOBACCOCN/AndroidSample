39a1c0349495e980dc086f6f3068d7077d6d6561
zyonghong <zyonghong@waterworld.com.cn>
2021-02-01 19:59:54 +0800

    add DefaultPreferenceActivity

---
+++ app/src/main/java/com/example/sample/activity/DefaultPreferenceActivity.java
@@ -0,0 +1,14 @@
+package com.example.sample.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DefaultPreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default_preference);
+    }
+}
