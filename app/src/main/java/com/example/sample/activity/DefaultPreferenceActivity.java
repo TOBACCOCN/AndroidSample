dfd07cddd59b1080ae77ac9a3dc76aaa9468b495
zyonghong <zyonghong@waterworld.com.cn>
2021-02-01 22:55:37 +0800

    add DefaultPreferenceActivity again

--- app/src/main/java/com/example/sample/activity/DefaultPreferenceActivity.java
+++ app/src/main/java/com/example/sample/activity/DefaultPreferenceActivity.java
@@ -1,8 +1,12 @@
 package com.example.sample.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 
 import android.os.Bundle;
+
+import com.example.sample.R;
 
 public class DefaultPreferenceActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class DefaultPreferenceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_default_preference);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame, new DefaultFragment()).commit();
     }
+
+    // this class must be public
+    public static class DefaultFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.default_preference, rootKey);
+            findPreference("ROM").setSummary("128G");
+            findPreference("RAM").setSummary("4G");
+        }
+    }
+
 }
